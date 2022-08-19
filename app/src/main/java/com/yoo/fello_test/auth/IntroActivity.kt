23f333ec5c55abc099ae619348310ce4e793dc32
heradenk <hera21denk@gmail.com>
2022-08-19 22:45:04 +0900

    first commit

---
+++ app/src/main/java/com/yoo/fello_test/auth/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.yoo.fello_test.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yoo.fello_test.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
