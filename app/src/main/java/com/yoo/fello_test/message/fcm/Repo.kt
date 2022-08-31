3f4e3b62efeb63359da61803260db86070bb37e2
heradenk <56153425+heradenk@users.noreply.github.com>
2022-08-31 23:32:46 +0900

    Update Repo.kt

--- app/src/main/java/com/yoo/fello_test/message/fcm/Repo.kt
+++ app/src/main/java/com/yoo/fello_test/message/fcm/Repo.kt
@@ -5,7 +5,7 @@ class Repo {
     companion object {
 
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAhMnhFVs:APA91bHow9tpxUZqbjJcE3meUylnEDs_cA3PzEXN9v46aNNtj0S1rTQhdsRNsMDqwEKsHGzqZ3PlYtkTpIrZFMX99LDwlOg-wfe7F5_4zP3psJPy5Dp1pfzedoOj8FpkCCMmx0EHoyvC"
+        const val SERVER_KEY = "SERVER_KEY"
         const val CONTENT_TYPE = "application/json"
 
     }
